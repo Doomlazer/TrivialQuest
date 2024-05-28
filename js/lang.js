5e34f260da817326f984f486c0c669d6e064dd43
DL <dl@thespr3.com>
2024-05-28 11:12:37 -0700

    bf

--- js/lang.js
+++ js/lang.js
@@ -148,23 +148,23 @@ function getLangStr(n) {
     if (n == 8) { // Police quest bonus string 2
         if (lang == "EN") {
             str = "here is a bonus of " + 
-                    (modeCompletionBonus[4]*pqQuestionsCorrect) + " points.\"";
+                    (5*pqQuestionsCorrect) + " points.\"";
             return str
         } else if (lang == "FR") {
             str = "voici un bonus de " + 
-                    (modeCompletionBonus[4]*pqQuestionsCorrect) + " points.\"";
+                    (5*pqQuestionsCorrect) + " points.\"";
             return str
         } else if (lang == "SP") {
             str = "aquí hay una bonificación de " + 
-                    (modeCompletionBonus[4]*pqQuestionsCorrect) + " puntos.\"";
+                    (5*pqQuestionsCorrect) + " puntos.\"";
             return str
         } else if (lang == "GR") {
             str = "hier gibt es einen Bonus von " + 
-                    (modeCompletionBonus[4]*pqQuestionsCorrect) + " Punkten.\"";
+                    (5*pqQuestionsCorrect) + " Punkten.\"";
             return str
         } else if (lang == "PL") {
             str = "oto bonus w wysokości " + 
-                    (modeCompletionBonus[4]*pqQuestionsCorrect) + " punktów.\"";
+                    (5*pqQuestionsCorrect) + " punktów.\"";
             return str
         }
     }
